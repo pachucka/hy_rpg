bc38a30c66088bc2a3ca65dbb7c2ccff0cad5dd2
pachucka <npachucka@gmail.com>
2023-12-19 00:27:10 +0100

    shooting and following enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBetweenShots;
+    public float start;
+    public GameObject projectile;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBetweenShots = start;
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        } else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if(timeBetweenShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBetweenShots = start;
+        } else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+    }
+}
