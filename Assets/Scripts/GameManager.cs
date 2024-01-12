492e6810160b922113ec7934e33d9bd875c3321c
pachucka <npachucka@gmail.com>
2024-01-13 00:30:57 +0100

    Revert "Revert "Revert "new switching scenes system"""
    This reverts commit 0dac9a27b6657cdc4f4fb2244cb777ed28c6ab75.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public bool menuOpen, dialogueActive;
-    [SerializeField] Animator transitionAnim;
 
 
     private void Start()
     {
-        if(instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else
-        {
-            Destroy(gameObject);
-        }
+        instance = this;
     }
 
     // controlling if Player can move
@@ -34,20 +25,5 @@ public class GameManager : MonoBehaviour
         {
             PlayerController.instance.canMove = true;
         }
-    }
-
-    public void NextLevel()
-    {
-        StartCoroutine(LoadLevel());
-    }
-
-    IEnumerator LoadLevel()
-    {
-        transitionAnim.SetTrigger("End");
-        menuOpen = true;
-        yield return new WaitForSeconds(1);
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-        transitionAnim.SetTrigger("Start");
-        menuOpen = false;
     }
 }
