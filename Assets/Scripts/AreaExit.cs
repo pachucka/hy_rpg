923d62c0c3419e91d899014abe02264079fa0306
pachucka <npachucka@gmail.com>
2024-01-13 00:31:00 +0100

    Revert "Revert "started repaiing menu""
    This reverts commit bf3bc6dacefd26e5f3ac1ed2c39a5056cbc4ffdb.

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -5,39 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class AreaExit : MonoBehaviour
 {
-    public string areaToLoad;
-    public string areaTransitionName;
-    public AreaEntrance entrance;
-
-    public float waitToLoad = 1f;
-    private bool shouldLoadAfterFade;
-
-    private void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        entrance.transitionName = PlayerController.instance.areaTransitionName;
-    }
-
-    private void Update()
-    {
-        if (shouldLoadAfterFade)
+        if (other.CompareTag("Player"))
         {
-            waitToLoad -= Time.deltaTime;
-            if(waitToLoad <= 0)
-            {
-                shouldLoadAfterFade = false;
-                SceneManager.LoadScene(areaToLoad);
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Player")
-        {
-            //SceneManager.LoadScene(areaToLoad);
-            shouldLoadAfterFade = true;
-            UIFade.instance.FadeToBlack();
-            PlayerController.instance.areaTransitionName = areaTransitionName;
+            GameManager.instance.NextLevel();
         }
     }
 }
