d59be2c09cdf2dd2874426359365c8091e69e977
pachucka <npachucka@gmail.com>
2024-01-24 11:13:26 +0100

    addded sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] music;
+    public Sound[] effects;
+
+    public AudioSource musicSource, effectsSource;
+}
