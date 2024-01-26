399f7bc01f99cf47823a9fdddd76d57b882484ea
pachucka <npachucka@gmail.com>
2024-01-26 16:35:57 +0100

    settings - resolution, quality and fullscreen

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,35 @@ public class SettingsController : MonoBehaviour
     public Slider musicSlider, effectsSlider;
 
     public GameObject soundPanel;
+
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    int currentResIndex = 0;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void ToggleMusic()
     {
@@ -43,5 +73,22 @@ public class SettingsController : MonoBehaviour
         {
             soundPanel.SetActive(false);
         }
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        Debug.Log(QualitySettings.GetQualityLevel());
+        QualitySettings.SetQualityLevel(qualityIndex);
+        Debug.Log(QualitySettings.GetQualityLevel());
+    }
+
+    public void ToogleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void setResolution(int resIndex)
+    {
+        Screen.SetResolution(resolutions[resIndex].width, resolutions[resIndex].height, Screen.fullScreen);
     }
 }
